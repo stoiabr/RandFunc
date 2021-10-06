e6450fc093c988c025a560bae0ddd48009c5dfb5
stoiabr <stoiabr@miamioh.edu>
2021-10-06 10:49:58 -0400

    adding line width parameter to the function along with comments in the header block and illustration with an example

--- PlotRandom.R
+++ PlotRandom.R
@@ -21,12 +21,17 @@
 ##     Mean_x = the sample mean of Random_values
 ##     SD_x = the sample standard deviation of Random_values
 
-plotRandomNormals <- function(numpts=1000, numbins=15, mu=24, sigma=3, meanColor="red", seed=8675309) {
+plotRandomNormals <- function(numpts=1000, 
+                              numbins=15, mu=24,
+                              sigma=3, 
+                              meanColor="red", 
+                              linewidth=2, 
+                              seed=8675309) {
   set.seed(seed)
   rand_x <- rnorm(numpts, mean=mu, sd=sigma)
   mean_x <- mean(rand_x)
   hist(rand_x, breaks=numbins)
-  abline(v=mean_x, col=meanColor)
+  abline(v=mean_x, col=meanColor, lwd=linewidth)
   list(Random_values = rand_x,
        Mean_x = mean_x,
        SD_x = sd(rand_x) )
